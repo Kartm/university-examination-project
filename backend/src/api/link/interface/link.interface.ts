2ef24442c57bb71af0318b19778e251d270e23ee
shreddZ99 <berkaymertkocak99@gmail.com>
2022-01-10 23:05:38 +0100

    Update link.interface.ts
    changed typed on participant_id

--- backend/src/api/link/interface/link.interface.ts
+++ backend/src/api/link/interface/link.interface.ts
@@ -1,7 +1,7 @@
 import {IIdHaver} from "../../../APIHelpers/IIdHaver";
 
 export interface LinkInterface extends IIdHaver {
-    participant_id?: number;
+    participant_id?: string;
     used?: boolean;
     sent_at?: string;
     link?: string;

d11cecedb025667ae04225f448efffc033191904
Łukasz Blachnicki <6399265+Kartm@users.noreply.github.com>
2022-01-17 19:57:28 +0100

    Owner editor template (#11)
    * Display template name in selector
    
    * Display template name in selector
    
    * Set header color
    
    * Set button size
    
    * Add structure of template exam
    
    * Move templates handling to store
    
    * Add creating exams from templates
    
    * Improve mock ids

--- frontend/src/pages/owner/create/create.screen.tsx
+++ frontend/src/pages/owner/create/create.screen.tsx
@@ -1,40 +1,65 @@
-import React, {useEffect} from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link, useHistory} from "react-router-dom";
 
 import Container from "../../../components/style/container.component";
 import Content from "../../../components/style/content.component";
 import Button from "../../../components/forms/button.component";
 import Text from "../../../components/style/text.component";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {updateTitleAction} from "../../../store/slices/ui.slice";
+import {Exam, ExamDraft} from "../../../models/exam.model";
+import {createExam, getExamTemplates, useExamTemplate} from "../../../store/slices/exam.slice";
+import {RootState} from "../../../store/configure.store";
 
 const CreateScreen = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const examState = useSelector((state: RootState) => state.exam);
+
+  useEffect(() => {
+    dispatch(getExamTemplates())
+  }, [])
 
   useEffect(() => {
     dispatch(updateTitleAction("Pass | Create an exam"));
   });
 
+  function handleSelectTemplate(title: string) {
+    const template = examState.examTemplates.find(t => t.title === title)!
+    // @ts-ignore
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    dispatch(useExamTemplate(template)).then(x => {
+      const exam: Exam = x.payload;
+      history.push(`/${exam.exam_uuid}/editor`);
+    })
+  }
+
+  function handleCreateExam() {
+    // @ts-ignore
+    dispatch(createExam()).then(x => {
+      const exam: Exam = x.payload;
+
+      history.push(`/${exam.exam_uuid}/editor`);
+    });
+  }
+
   return (
     <Container>
       <Content>
-        <Text h1 style={{ marginBottom: 20 }}>
-          Create screen
-        </Text>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <select onChange={(e) => handleSelectTemplate(e.target.value)}>
+            <option disabled selected>Select a template</option>
+            {examState.examTemplates.map((value, i) =>
+              <option key={i} value={value.title}>{value.title} - {value.questions.length} questions</option>)
+            }
+          </select>
+        </div>
 
-        <Text h2 style={{ marginBottom: 20 }}>
-          [TODO: select template]
-        </Text>
-        <Text h2 style={{ marginBottom: 20 }}>
+        <Text style={{ marginTop: 20, marginBottom: 20 }}>
           or
         </Text>
-        {/* TODO this is hardcoded */}
-        <Link
-          to="/123e4567-e89b-12d3-a456-426652340000/editor"
-          style={{ marginRight: 10 }}
-        >
-          <Button text="Create from scratch" color="primary" />
-        </Link>
+
+        <Button text="Create from scratch" color="primary" onClick={handleCreateExam} />
       </Content>
     </Container>
   );

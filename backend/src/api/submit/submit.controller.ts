4725eeed5795a97c068831dfe72b84e3e80426df
shreddZ99 <berkaymertkocak99@gmail.com>
2022-01-23 22:12:50 +0100

    submit function

--- backend/src/api/submit/submit.controller.ts
+++ backend/src/api/submit/submit.controller.ts
@@ -1,5 +1,8 @@
-import {Controller, Get, Param, Post, Put} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post, Put} from "@nestjs/common";
 import {SubmitService} from "./submit.service";
+import {testEntity} from "../../entity/test.entity";
+import {QuestionAnswerService} from "../questionAnswer/questionAnswer.service";
+import {questionAnswerEntity} from "../../entity/questionAnswer.entity";
 
 @Controller()
 export class SubmitController {
@@ -7,9 +10,9 @@ export class SubmitController {
     constructor(private submitService: SubmitService) {
     }
 
- @Get(":question_id")
-   submitAnswer(@Param("question_id") id: string){
-      return this.submitService.submitAnswer();
+ @Post()
+   submitAnswer(@Body() test: testEntity, @Body() answers: questionAnswerEntity[]){
+      return this.submitService.submitAnswer(test,answers);
     }
 
 
--- backend/src/api/submit/submit.service.ts
+++ backend/src/api/submit/submit.service.ts
@@ -1,40 +1,53 @@
 import {Injectable} from "@nestjs/common";
-import {TestInterface} from "../test/interfaces/test.interface";
-import {SubmitInterface} from "./interface/submit.interface";
-import {settingsEntity} from "../../entity/settings.entity";
-import {ownerEntity} from "../../entity/owner.entity";
+
+import {testEntity} from "../../entity/test.entity";
+import {questionAnswerEntity} from "../../entity/questionAnswer.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+
+
 
 @Injectable()
-export class SubmitService implements SubmitInterface{
+export class SubmitService {
 
-    id: string;
-    name: string;
-    owner: ownerEntity;
-    owner_link: string;
-    setting: settingsEntity;
-    time_end: Date;
-    time_start: Date;
-
-
-
-    checkTime() {
-       const start=  Math.round(this.time_start.getDate() /1000);
-       const end=   Math.round(this.time_end.getDate() /1000)
-        const diff = end - start;
-        if (diff>0)
-        {
-            return true
-        } else
-        {
-            false; }
-    }
-
-    submitAnswer() {
-        if 
-
+    constructor(@InjectRepository(questionAnswerEntity) private questionAnswerRepository: Repository<questionAnswerEntity>)
+    {
 
     }
 
 
 
+
+    checkTime(test: testEntity) {
+       // const start=  Math.round(new Date().getDate() /1000);
+       // const end=   Math.round(test.time_end.getDate() /1000)
+       //  const diff = end - start;
+       if(new Date()>test.time_end){
+           return true;
+       }
+        // if (diff>0)
+        // {
+        //     return true;
+          else {
+            return false; }
+    }
+
+    submitAnswer(test: testEntity, answers: questionAnswerEntity[]) {
+        if(this.checkTime(test))
+        {
+            this.submitQuestionAnswerToDatabase(answers);
+
+        } else {
+            return alert("Exam has finished, sorry");
+        }
+
+
+
+    }
+
+
+    private submitQuestionAnswerToDatabase(answers: questionAnswerEntity[]) {
+     this.questionAnswerRepository.save(answers)
+
+    }
 }

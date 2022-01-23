fabbb2bd83fb0045c176cca77d377070ce0c8f99
shreddZ99 <berkaymertkocak99@gmail.com>
2022-01-23 21:28:52 +0100

    wpi-submit

--- backend/src/api/submit/submit.service.ts
+++ backend/src/api/submit/submit.service.ts
@@ -1,21 +1,40 @@
 import {Injectable} from "@nestjs/common";
 import {TestInterface} from "../test/interfaces/test.interface";
+import {SubmitInterface} from "./interface/submit.interface";
+import {settingsEntity} from "../../entity/settings.entity";
+import {ownerEntity} from "../../entity/owner.entity";
 
 @Injectable()
-export class SubmitService implements TestInterface {
+export class SubmitService implements SubmitInterface{
+
+    id: string;
+    name: string;
+    owner: ownerEntity;
+    owner_link: string;
+    setting: settingsEntity;
+    time_end: Date;
+    time_start: Date;
 
 
-    checkTime(){
+
+    checkTime() {
+       const start=  Math.round(this.time_start.getDate() /1000);
+       const end=   Math.round(this.time_end.getDate() /1000)
+        const diff = end - start;
+        if (diff>0)
+        {
+            return true
+        } else
+        {
+            false; }
+    }
+
+    submitAnswer() {
+        if 
+
 
     }
 
-    submitAnswer(){
-        let isGoing = this.time_end;
-        let hasFinished = this.time_start;
 
-
-
-
-    }
 
 }

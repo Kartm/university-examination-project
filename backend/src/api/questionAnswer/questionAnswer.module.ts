263c9145b9b7b4b09a5244b0d0dac435c591ca0d
Jakub Seredyński <53041150+sereczeq@users.noreply.github.com>
2022-01-24 12:20:42 +0100

    It started (#24)
    * WIP
    
    * Fixed result service after merge
    
    * wip-submit
    
    * Update link.controller.ts
    
    * Updatind results because of types
    
    * Update question.service.ts
    
    * Deleted /shared/database because it wasn't used anywhere
    
    * Update test.service.ts
    
    * Update test.entity.ts
    
    * Update question.controller.ts
    
    * Update test.entity.ts
    
    * wpi-submit
    
    * submit function
    
    * Update questionAnswer.service.ts
    
    * Update questionChoice.service.ts
    
    * Update questionChoice.controller.ts
    
    * updated template.service
    
    * Fixed results service after merge
    
    * template fix
    
    * I made it start. Probably doesn't work anyways
    
    Co-authored-by: shreddZ99 <berkaymertkocak99@gmail.com>

--- backend/src/api/questionAnswer/questionAnswer.module.ts
+++ backend/src/api/questionAnswer/questionAnswer.module.ts
@@ -1,8 +1,14 @@
 import {Module} from "@nestjs/common";
 import {QuestionAnswerController} from "./questionAnswer.controller";
 import {QuestionAnswerService} from "./questionAnswer.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {testEntity} from "../../entity/test.entity";
+import {questionAnswerEntity} from "../../entity/questionAnswer.entity";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([questionAnswerEntity])
+    ],
     controllers:[QuestionAnswerController],
     providers:[QuestionAnswerService]
 })
--- backend/src/api/questionChoice/questionChoice.module.ts
+++ backend/src/api/questionChoice/questionChoice.module.ts
@@ -1,10 +1,13 @@
 import {Module} from "@nestjs/common";
 import {QuestionChoiceController} from "./questionChoice.controller";
 import {QuestionChoiceService} from "./questionChoice.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {testEntity} from "../../entity/test.entity";
+import {questionChoiceEntity} from "../../entity/questionChoice.entity";
 
 @Module({
     imports: [
-
+        TypeOrmModule.forFeature([questionChoiceEntity])
     ],
     controllers:[QuestionChoiceController],
     providers:[QuestionChoiceService]
